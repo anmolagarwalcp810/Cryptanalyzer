307ac00b63293b107ddaf43ecd7d155beab9a42f
Anmol Agarwal <anmol.agarwalcp810@gmail.com>
2022-01-19 12:07:13 +0530

    Increased dictionary size by using another dictionary

--- read_dictionary.cpp
+++ read_dictionary.cpp
@@ -16,6 +16,24 @@ void solve(string&line){
 	s.insert(temp);
 }
 
+void convert_to_upper(string&line){
+	int n = line.size();
+	for(int i=0;i<n;i++){
+		line[i] = toupper(line[i]);
+	}
+}
+
+void solve2(string&line){
+	int n = line.size(), i=0;
+	convert_to_upper(line);
+	string temp = "";
+	while(i<n && line[i]>='A' && line[i]<='Z'){
+		temp+=line[i];
+		i++;
+	}
+	s.insert(temp);
+}
+
 int main(){
 	// TODO: take file name as argument
 	ifstream file;
@@ -24,6 +42,12 @@ int main(){
 	while(file){
 		getline(file,line);
 		solve(line);
+	}
+	file.close();
+	file.open("words.txt");
+	while(file){
+		getline(file,line);
+		solve2(line);
 	}
 	file.close();
 	ofstream file2;
